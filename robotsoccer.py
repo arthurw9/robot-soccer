cc7ba6fe7a70ba303788bc27548174231a72a4a1
Arthur Weinberger <arthurweinberger@gmail.com>
2013-12-21 20:24:28 -0800

    Added a tutorial with sample robots.  Created constants for the board size. Explicity set the robot start locations. Created svg rectangles for the goals. Fixed a bug with how the ball moves. Explicity set when each robot takes its turn.

--- robotsoccer.py
+++ robotsoccer.py
@@ -12,8 +12,16 @@ class MainPage(webapp2_helper.RequestHandler2):
     self.response.write(template.render(template_data));
 
 
+class TutorialPage(webapp2_helper.RequestHandler2):
+  
+  def get2(self, template_data):
+    template = webapp2_helper.JINJA_ENVIRONMENT.get_template('tutorial.html')
+    self.response.write(template.render(template_data));
+
+
 application = webapp2.WSGIApplication(
     [ ('/', MainPage),
+      ('/tutorial', TutorialPage),
       ('/list_robots', edit_robots.ListRobots),
       ('/new_robot', edit_robots.NewRobot),
       ('/edit', edit_robots.EditRobot),
